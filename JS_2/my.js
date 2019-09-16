1c6080fd4c160214ceca76f88d953f8ae57db7eb
savchyn-vasyl <bookeryx@gmail.com>
2019-09-16 20:05:50 +0300

    16.09

---
+++ JS_2/my.js
@@ -0,0 +1,12 @@
+var sum = 0, counter = 1;
+while (conter <= 10) {
+sum = sum + counter; // sum += counter
+counter = counter + 1; // counter++
+}
+console.log (sum)
+
+var sum1 = 0, counter1;
+for ( counter1 = 1; counter1 <= 10; counter1++) {
+    sum1 += counter1;
+    }
+console.log (sum1, counter1)
